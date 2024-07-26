c5fabd94acc07f438ecf5987f6c87a4e0cfb09b0
Joko <kalle0045@gmail.com>
2024-07-26 10:26:04 +0200

    clean up nav.tsx and page.tsx

--- src/app/[id]/page.tsx
+++ src/app/[id]/page.tsx
@@ -1,58 +1,83 @@
 import axios from "axios";
 import Builder from "@/component/Builder";
 
+const headers = {
+  Authorization: 'Bearer ' + '795ae4f8716b1e03b49e9c137966829a711209f2c20226a2ee3db1ae5b04012b7e4017ee8795c470d5d599009a6dae824a71d580a50280a1733b6881db90a3dfdaeb833b033ff12c97173931ac5e540e4a52a0ed2bb26768eab70a233e5ee3c7dd6c095e9c9efbe5ed10012d884ea7f0cbfb94d4455358d25fe54274757b9e26',
+};
+
 async function getData(id: string | number) {
+  try {
+    // GET PAGE ID
+    const requestUrlNav = `https://headless.mapztour.de/api/setting?populate=deep`;
+    const responseUrlNav = await axios.get(requestUrlNav, { headers });
+    const navData = responseUrlNav.data.data.attributes.nav;
 
-  const headers = {
-    Authorization: 'Bearer ' + '795ae4f8716b1e03b49e9c137966829a711209f2c20226a2ee3db1ae5b04012b7e4017ee8795c470d5d599009a6dae824a71d580a50280a1733b6881db90a3dfdaeb833b033ff12c97173931ac5e540e4a52a0ed2bb26768eab70a233e5ee3c7dd6c095e9c9efbe5ed10012d884ea7f0cbfb94d4455358d25fe54274757b9e26',
-  };
+    const mainItem = findObjectByKeyValue(navData, "link", id);
+    let getID = mainItem?.page?.data?.id;
 
-  // GET PAGE ID
-  const requestUrlNav = `https://headless.mapztour.de/api/setting?populate=deep`
-
-  const responseUrlNav = await axios.get(requestUrlNav, {headers});
-  const navData = responseUrlNav.data.data.attributes.nav
-  // console.log(navData);
-
-  function findObjectByKeyValue(objects: any, key: string | number, value: string | number) {
-    for (const obj of objects) {
-      if (searchObject(obj, key, value)) {
-        return obj;
+    if (mainItem && mainItem.submenu) {
+      const submenuItem = findObjectByKeyValue(mainItem.submenu, "link", id);
+      if (submenuItem) {
+        getID = submenuItem.id;
+      } else {
+        console.log("No submenu item found with the specified link.");
       }
+    } else {
+      console.log("No main menu item found with the specified link or it has no submenu.");
     }
-    return null;
+
+    if (!getID) {
+      throw new Error("No valid ID found.");
+    }
+
+    // GET PAGE
+    const requestUrl = `https://headless.mapztour.de/api/pages/${getID}?populate=deep`;
+    const response = await axios.get(requestUrl, { headers });
+    return response.data.data.attributes.cms;
+  } catch (error) {
+    console.error("Error fetching data:", error);
+    throw error;
   }
+}
 
-  function searchObject(obj: any, key: string | number, value: string | number) {
-    if (typeof obj !== 'object' || obj === null) {
-      return false;
+function findObjectByKeyValue(objects: any[], key: string | number, value: string | number) {
+  for (const obj of objects) {
+    if (searchObject(obj, key, value)) {
+      return obj;
     }
-    if (obj[key] === value) {
-      return true;
-    }
-    for (const k in obj) {
-      if (obj.hasOwnProperty(k) && searchObject(obj[k], key, value)) {
-        return true;
-      }
-    }
+  }
+  return null;
+}
+
+function searchObject(obj: any, key: string | number, value: string | number): boolean {
+  if (typeof obj !== 'object' || obj === null) {
     return false;
   }
-
-  const result = findObjectByKeyValue(navData, "link", id);
-
-  // GET PAGE
-  const requestUrl = `https://headless.mapztour.de/api/pages/${result.page.data.id}?populate=deep`
-  const response = await axios.get(requestUrl, { headers });
-  console.log(response.data.data.attributes.cms);
-  return response.data.data.attributes.cms;
+  if (obj[key] === value) {
+    return true;
+  }
+  for (const k in obj) {
+    if (obj.hasOwnProperty(k) && searchObject(obj[k], key, value)) {
+      return true;
+    }
+  }
+  return false;
 }
 
 export default async function Home({ params }: { params: { id: string } }) {
-  const data = await getData(params.id)
+  try {
+    const data = await getData(params.id);
 
-  return (
-      <main className="">
-        <Builder data={data}/>
-      </main>
-  );
+    return (
+        <main className="">
+          <Builder data={data} />
+        </main>
+    );
+  } catch (error) {
+    return (
+        <main className="">
+          <p>Error loading data. Please try again later.</p>
+        </main>
+    );
+  }
 }
--- src/component/Nav.tsx
+++ src/component/Nav.tsx
@@ -1,35 +1,55 @@
 import axios from "axios";
 import Link from "next/link";
 
-async function getData(id: string | number) {
-    const requestUrl = `https://headless.mapztour.de/api/setting?populate=deep`
+interface NavItem {
+    id: number;
+    title: string;
+    link: string;
+    submenu?: NavItem[];
+}
+
+async function getData(): Promise<NavItem[]> {
+    const requestUrl = `https://headless.mapztour.de/api/setting?populate=deep`;
     const headers = {
         Authorization: 'Bearer ' + '795ae4f8716b1e03b49e9c137966829a711209f2c20226a2ee3db1ae5b04012b7e4017ee8795c470d5d599009a6dae824a71d580a50280a1733b6881db90a3dfdaeb833b033ff12c97173931ac5e540e4a52a0ed2bb26768eab70a233e5ee3c7dd6c095e9c9efbe5ed10012d884ea7f0cbfb94d4455358d25fe54274757b9e26',
     };
-    const response = await axios.get(requestUrl, {headers});
-    // console.log(response.data.data.attributes.nav);
-    return response.data.data.attributes.nav;
+
+    try {
+        const response = await axios.get(requestUrl, { headers });
+        return response.data.data.attributes.nav;
+    } catch (error) {
+        console.error("Error fetching navigation data:", error);
+        throw new Error("Failed to fetch navigation data");
+    }
 }
 
-const Nav = async ({params}: any) => {
-    const data = await getData(params)
+const Nav = async ({ params }: { params: { id: string } }) => {
+    const data = await getData();
+
     return (
         <nav>
             <ul>
-                {
-                    data.map((item: any) => {
-                            return <li key={item.id}>
-                                <Link href={item.link}>
-                                    {item.title}
-                                </Link>
-                            </li>
-
-                        }
-                    )
-                }
+                {data.map((item) => (
+                    <li key={item.id}>
+                        <Link href={item.link}>
+                            {item.title}
+                        </Link>
+                        {item.submenu && item.submenu.length > 0 && (
+                            <ul>
+                                {item.submenu.map((subItem) => (
+                                    <li key={subItem.id}>
+                                        <Link href={subItem.link}>
+                                            {subItem.title}
+                                        </Link>
+                                    </li>
+                                ))}
+                            </ul>
+                        )}
+                    </li>
+                ))}
             </ul>
         </nav>
-    )
-}
+    );
+};
 
 export default Nav;
