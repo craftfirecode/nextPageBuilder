df4d067ce1f551bbd06a7a7c82632a5d49fef108
Joko <kalle0045@gmail.com>
2024-08-02 20:10:44 +0200

    submenu fix

--- next/src/component/RichText.tsx
+++
@@ -1,8 +0,0 @@
-import {BlocksRenderer} from "@strapi/blocks-react-renderer";
-import Link from "next/link";
-
-const RichText = ({content}: any) => {
-    return <BlocksRenderer content={content} />;
-}
-
-export default RichText;
