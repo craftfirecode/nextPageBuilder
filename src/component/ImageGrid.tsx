e668ff3c3c31b3cf17cf78339a056e5cc57f8f64
Joko <kalle0045@gmail.com>
2024-08-10 20:38:34 +0200

    Cookie multi lang

--- src/component/ImageGrid.tsx
+++ src/component/ImageGrid.tsx
@@ -8,7 +8,7 @@ const ImageGrid = ({ cms }: any) => {
             {data.map((item: any, index: number) => {
                 const rowCapacity = 3;
                 const itemsInRow = Math.min(data.length - Math.floor(index / rowCapacity) * rowCapacity, rowCapacity);
-                const widthClass = itemsInRow === 1 ? 'w-full' : (itemsInRow === 2 ? 'w-1/2' : 'w-1/3');
+                const widthClass = itemsInRow === 1 ? 'w-full' : (itemsInRow === 2 ? 'md:w-full lg:w-1/2' : 'md:w-full lg:w-1/3');
 
                 return (
                     <div key={index} className={`${widthClass} px-2 mb-4`}>
