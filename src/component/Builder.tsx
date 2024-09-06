53bf5ea66567bc1ad1598bf887919a571f6dc7d8
jokoson <deine.email@example.com>
2024-09-06 08:58:51 +0200

    refactor: Add CodeBlock component for syntax highlighting in CMS

--- src/component/Builder.tsx
+++ src/component/Builder.tsx
@@ -1,8 +1,8 @@
-import React from 'react';
+import React from "react";
 import HeadlineView from "@/component/HeadlineView";
 import AccordionView from "@/component/AccordionView";
 import ButtonView from "@/component/ButtonView";
-import {CarouselView} from "@/component/CarouselView";
+import { CarouselView } from "@/component/CarouselView";
 import ContentView from "@/component/ContentView";
 import ContentImageView from "@/component/ContentImageView";
 import HeroView from "@/component/HeroView";
@@ -13,102 +13,110 @@ import Image from "next/image";
 import ImageGrid from "@/component/ImageGrid";
 import MarginView from "@/component/MarginView";
 import RatingView from "@/component/RatingView";
+import CodeBlock from "./CodeBlock";
 
 const Builder = (prop: any) => {
-    // console.log(prop.data);
-    const renderComponent = (component: any) => {
-        switch (component.__component) {
-            case 'cms.accordion':
-                return (
-                    <div className="">
-                        <AccordionView cms={component}/>
-                    </div>
-                );
-            case 'cms.rating':
-                return (
-                    <div className="">
-                        <RatingView cms={component}/>
-                    </div>
-                );
-            case 'cms.margin':
-                return (
-                    <div className="">
-                        <MarginView cms={component}/>
-                    </div>
-                );
-            case 'cms.headline':
-                return (
-                    <div className="">
-                        <HeadlineView cms={component}/>
-                    </div>
-                );
-            case 'cms.modal':
-                return (
-                    <div className="">
-                        <ModalView cms={component}/>
-                    </div>
-                );
-            case 'cms.carousel':
-                return (
-                    <div className="">
-                        <CarouselView cms={component.carousel}/>
-                    </div>
-                );
-            case 'cms.image-grid':
-                return (
-                    <div className="">
-                        <ImageGrid cms={component}/>
-                    </div>
-                );
-            case 'cms.content':
-                return (
-                    <div className="">
-                        <ContentView cms={component}/>
-                    </div>
-                );
-            case 'cms.content-image':
-                return (
-                    <div className="">
-                        <ContentImageView cms={component}/>
-                    </div>
-                );
-            case 'cms.v-hero':
-                return (
-                    <div className="">
-                        <VHeroView cms={component}/>
-                    </div>
-                );
-            case 'cms.button':
-                return (
-                    <div className="">
-                        <ButtonView cms={component}/>
-                    </div>
-                );
-            case 'cms.post-category':
-                return (
-                    <div className="">
-                        <PostView cms={component}/>
-                    </div>
-                );
-            case 'cms.hero':
-                return (
-                    <div className="">
-                        <HeroView cms={component}/>
-                    </div>
-                );
-            default:
-                return null;
-        }
-    };
+  // console.log(prop.data);
+  const renderComponent = (component: any) => {
+    switch (component.__component) {
+      case "cms.accordion":
+        return (
+          <div className="">
+            <AccordionView cms={component} />
+          </div>
+        );
+      case "cms.rating":
+        return (
+          <div className="">
+            <RatingView cms={component} />
+          </div>
+        );
+      case "cms.margin":
+        return (
+          <div className="">
+            <MarginView cms={component} />
+          </div>
+        );
+      case "cms.headline":
+        return (
+          <div className="">
+            <HeadlineView cms={component} />
+          </div>
+        );
+      case "cms.modal":
+        return (
+          <div className="">
+            <ModalView cms={component} />
+          </div>
+        );
+      case "cms.carousel":
+        return (
+          <div className="">
+            <CarouselView cms={component.carousel} />
+          </div>
+        );
+      case "cms.image-grid":
+        return (
+          <div className="">
+            <ImageGrid cms={component} />
+          </div>
+        );
+      case "cms.content":
+        return (
+          <div className="">
+            <ContentView cms={component} />
+          </div>
+        );
+      case "cms.content-image":
+        return (
+          <div className="">
+            <ContentImageView cms={component} />
+          </div>
+        );
+      case "cms.v-hero":
+        return (
+          <div className="">
+            <VHeroView cms={component} />
+          </div>
+        );
+      case "cms.button":
+        return (
+          <div className="">
+            <ButtonView cms={component} />
+          </div>
+        );
+      case "cms.post-category":
+        return (
+          <div className="">
+            <PostView cms={component} />
+          </div>
+        );
+      case "cms.hero":
+        return (
+          <div className="">
+            <HeroView cms={component} />
+          </div>
+        );
+      case "cms.code-block":
+        return (
+          <div className="">
+            <CodeBlock cms={component} />
+          </div>
+        );
+      default:
+        return null;
+    }
+  };
 
-    return (
-        <>
-            {prop.data.map((component: any, index: any) => (
-                <React.Fragment key={index}>
-                    {renderComponent(component)}
-                </React.Fragment>
-            ))}
-        </>);
-}
+  return (
+    <>
+      {prop.data.map((component: any, index: any) => (
+        <React.Fragment key={index}>
+          {renderComponent(component)}
+        </React.Fragment>
+      ))}
+    </>
+  );
+};
 
 export default Builder;
---
+++ src/component/CodeBlock.tsx
@@ -0,0 +1,17 @@
+import SyntaxHighlighter from "react-syntax-highlighter";
+import { anOldHope } from "react-syntax-highlighter/dist/esm/styles/hljs";
+
+import { BlocksRenderer } from "@strapi/blocks-react-renderer";
+
+const CodeBlock = ({ cms }: { cms: any }) => {
+  console.log(cms.code);
+
+  return (
+    <div className="font-mono">
+      <SyntaxHighlighter language="javascript" style={anOldHope}>
+        {cms.code}
+      </SyntaxHighlighter>
+    </div>
+  );
+};
+export default CodeBlock;
