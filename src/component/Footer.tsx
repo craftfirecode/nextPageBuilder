f9b68dd38f0019a8ba5b396d2571fb6ac308e67d
jokoson <deine.email@example.com>
2024-09-09 09:34:02 +0200

    refactor: Update margin in Footer component

--- src/component/Footer.tsx
+++ src/component/Footer.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 const Footer = () => {
     return (
         <footer className="border-t text-black mt-5">
-            <div className="mt-2 flex gap-2">
+            <div className="py-5 flex gap-2">
                 <Link className="hover:underline" href="/impressum">Impressum</Link>
                 <div>&</div>
                 <Link className="hover:underline" href="/datenschutz">Datenschutz</Link>
