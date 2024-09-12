2925e0b07f337a7755fb0924aa816c21fbe90987
jokoson <deine.email@example.com>
2024-09-12 18:00:44 +0200

    Refactor tailwind.config.ts to update breakpoint values

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -21,8 +21,8 @@ const config: Config = {
       sm: '640px',
       md: '768px',
       lg: '1024px',
-      xl: '1280px',
-      '2xl': '1536px',
+      xl: '1180px',
+      '2xl': '1180px',
     },
     extend: {
       fontFamily: {
